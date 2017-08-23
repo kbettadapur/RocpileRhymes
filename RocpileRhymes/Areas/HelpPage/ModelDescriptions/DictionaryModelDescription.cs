d6205bd99f6901c7a3668c08228b4cb31d6a8170
Kirin Bettadapur <kirinbetta97@gmail.com>
2017-08-22 23:49:43 -0400

    init

---
+++ RocpileRhymes/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace RocpileRhymes.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ RocpileRhymes/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace RocpileRhymes.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
